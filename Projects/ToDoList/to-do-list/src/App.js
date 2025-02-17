c872ee6e312ca30aeddf225db67c0575ab400cb4
vikashsingh-15 <writetovikashsingh@gmail.com>
2025-02-17 20:05:55 +0530

    Add UserContext and implement App structure with Login and Profile components

---
+++ Projects/ToDoList/to-do-list/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ToDoList from "./Components/ToDoList";
+import ToDoList2 from "./Components/ToDOList2";
+
+function App() {
+  return (
+    <>
+      {/* <ToDoList /> */}
+      <ToDoList2 />
+    </>
+  );
+}
+
+export default App;
--- Projects/project-one/src/App.js
+++ Projects/project-one/src/App.js
@@ -1,51 +1,25 @@
-// import "./App.css";
-// import React from "react";
-// import Ecomm from "./pro/ecomm";
-// import ToDoList from "./pro/ToDoList";
-// import ToDoList2 from "./pro/ToDOList2";
-// import Options from "./pro/options";
-// import LengthChecker from "./pro/lengthChecker";
-// import FormClass from "./pro/FormClass";
-// import FormFunction from "./pro/FormFunction";
-// import BallRefClass from "./pro/BallRef";
-
-// function App() {
-//   return (
-//     <>
-//       {/* <Ecomm /> */}
-//       {/* <ToDoList /> */}
-
-//       {/* <ToDoList2 /> */}
-
-//       {/* <Options /> */}
-
-//       {/* <LengthChecker /> */}
-//       {/* <FormClass /> */}
-
-//       {/* <FormFunction /> */}
-
-//       {/* <BallRefClass /> */}
-//     </>
-//   );
-// }
-
-// export default App;
-
-//////////////////////////CONTEXT API//////////////////////////
-
-import React from "react";
 import "./App.css";
-import Login from "./pro/ContexAPii/Components/Login";
-import Profile from "./pro/ContexAPii/Components/Profile";
-import UserContextProvider from "./pro/ContexAPii/UserContextProvider";
+import React from "react";
+import Ecomm from "./pro/ecomm";
+import Options from "./pro/options";
+import LengthChecker from "./pro/lengthChecker";
+import FormClass from "./pro/FormClass";
+import FormFunction from "./pro/FormFunction";
+import BallRefClass from "./pro/BallRef";
 
 function App() {
   return (
-    <UserContextProvider>
-      <h1>ContextAPi</h1>
-      <Login />
-      <Profile />
-    </UserContextProvider>
+    <>
+      {/* <Ecomm /> */}
+      {/* <Options /> */}
+
+      {/* <LengthChecker /> */}
+      {/* <FormClass /> */}
+
+      {/* <FormFunction /> */}
+
+      {/* <BallRefClass /> */}
+    </>
   );
 }
 
