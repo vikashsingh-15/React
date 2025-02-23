392562f9659a27aea0998cd78606700365b05f08
vikashsingh-15 <writetovikashsingh@gmail.com>
2025-02-24 01:59:03 +0530

    commmit

--- Projects/ContexApi/theme-switcher/src/App.js
+++ Projects/ContexApi/theme-switcher/src/App.js
@@ -1,3 +1,45 @@
+// import React, { useContext } from "react";
+// import "./App.css";
+
+// import { ThemeContext } from "./Contex/ThemeProvider";
+// import Profile from "./Components/Profile";
+
+// function App() {
+//   const context = useContext(ThemeContext);
+//   return (
+//     <div className="App">
+//       <h2>Theme Switcher</h2>
+//       <p>Lets break leg</p>
+//       <Profile />
+//       <p>My theme is {context}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//// way 2 --> we dont have to useContect in different file we can use it in the same file (theme Provider)
+// import React from "react";
+// import "./App.css";
+
+// import { Context } from "./Contex/ThemeProvider";
+// import Profile from "./Components/Profile";
+
+// function App() {
+//   const theme = Context();
+//   return (
+//     <div className="App">
+//       <h2>Theme Switcher</h2>
+//       <p>Lets break leg</p>
+//       <Profile />
+//       <p>My theme is {theme}</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+// -----------------THEME TOGLER CODE --------------------
+
 import React from "react";
 import "./App.css";
 
@@ -5,15 +47,23 @@ import { Context } from "./Contex/ThemeProvider";
 import Profile from "./Components/Profile";
 
 function App() {
-  const theme = Context();
+  const { toggle, handleToggle } = Context();
+  console.log(toggle);
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: toggle ? "#333" : "#fff",
+        color: toggle ? "#fff" : "#000",
+      }}
+    >
+      {/* first braces because style will have JavaScript and second beaces because it taks object */}
       <h2>Theme Switcher</h2>
       <p>Lets break leg</p>
       <Profile />
-      <p>My theme is {theme}</p>
+      <p>My theme is {toggle ? "Dark" : "Light"}</p>
+      <button onClick={handleToggle}>Change Theme</button>
     </div>
   );
 }
-
 export default App;
--- Projects/ContexApi/theme-switcher/src/Contex/ThemeProvider.js
+++ Projects/ContexApi/theme-switcher/src/Contex/ThemeProvider.js
@@ -1,15 +1,55 @@
-import React, { createContext, useContext } from "react";
+// import React, { createContext } from "react";
 
-export const ThemeContext = createContext();
-// const ThemeContext = React.createContext();// another way to create context when you dont import createContext from 'react'
+// export const ThemeContext = createContext();
+// // const ThemeContext = React.createContext();// another way to create context when you dont import createContext from 'react'
+
+// function ThemeProvider({ children }) {
+//   return (
+//     <ThemeContext.Provider value="light">{children}</ThemeContext.Provider>
+//   );
+// }
+
+// export default ThemeProvider;
+
+// --------way 2 --> here useContext is used in the same file (theme Provider)--------------------
+
+// import React, { createContext, useContext } from "react";
+
+// export const ThemeContext = createContext();
+
+// export const Context = () => {
+//   return useContext(ThemeContext);
+// };
+
+// function ThemeProvider({ children }) {
+//   return (
+//     <ThemeContext.Provider value="light">{children}</ThemeContext.Provider>
+//   );
+// }
+
+// export default ThemeProvider;
+
+// -----------------THEME TOGLER CODE --------------------
+
+import React, { createContext, useContext, useState } from "react";
+
+export const ThemeContext = createContext(false);
 
 export const Context = () => {
   return useContext(ThemeContext);
 };
 
 function ThemeProvider({ children }) {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <ThemeContext.Provider value="light">{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ toggle, handleToggle }}>
+      {children}
+    </ThemeContext.Provider>
   );
 }
 
